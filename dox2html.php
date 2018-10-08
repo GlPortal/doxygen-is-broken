caf7c36e044c1936380e30aaf48e096e67f3e809
Henry Hirsch <henry@w3-net.de>
2018-10-08 20:37:54 +0200

    Add simple member documentation

--- dox2html.php
+++ dox2html.php
@@ -2,6 +2,7 @@
 $xml=simplexml_load_file("example.xml") or die("Error: Cannot create object");
 //var_dump();
 $className = $xml->compounddef->compoundname[0];
+$definitions = $xml->compounddef;
 include('template.php');
 //echo $xml->doxygen->compounddef . "<br>";
 ?>
--- template.php
+++ template.php
@@ -1,11 +1,30 @@
 <html>
 <head>
 <title>
-Class documentation for <?php echo $className ?>
+Class Documentation for <?php echo $className ?>
 </title>
 </head>
 <h1>
-  <?php echo $className ?>
+  Class Documentation for <?php echo $className ?>
 
 </h1>
+<?php
+foreach ($definitions->sectiondef as $section) {
+    if($section['kind'] == 'private-static-attrib'){
+        echo '<h2>Private Members</h2>', PHP_EOL;
+    } else if ($section['kind'] == 'public-static-func') {
+        echo '<h2>Public Members</h2>', PHP_EOL;
+    }
+    echo '<p>', PHP_EOL;
+    echo '<ul>', PHP_EOL;
+    foreach ($section->memberdef as $member) {
+        echo '<li>', PHP_EOL;
+        echo $member->type, ' ', $member->name, PHP_EOL;
+        echo '</li>', PHP_EOL;
+    }
+    echo '</ul>', PHP_EOL;
+    echo '</p>', PHP_EOL;
+}
+
+?>
 </html>
