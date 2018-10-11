d9a42e25ffd98146600d5d55b5f000e06f16d08e
Henry Hirsch <henry@w3-net.de>
2018-10-11 21:35:48 +0200

    More verbose error message when file can't be loaded

--- dox2html.php
+++ dox2html.php
@@ -1,6 +1,6 @@
 <?php
 include('doxygenToHtml.php');
-$xml=simplexml_load_file("example.xml") or die("Error: Cannot create object");
+$xml=simplexml_load_file("example.xml") or die("Error: Unable to load file.");
 $className = $xml->compounddef->compoundname[0];
 $definitions = $xml->compounddef;
 include('template.php');
